4081ed7895bc6c8ca5e0d2d44c4aa49d27bd0f05
Abbas Merchant <abbasmerchant60@gmail.com>
2023-10-21 19:42:38 +0530

    Abbas initiated spinner, image click and added 2 intents, one for qr code and one for book borrrowed. Also added intent in login page to open up the main page to be shown. Abbas is the best

--- app/src/main/java/com/example/mad_project/library.java
+++ app/src/main/java/com/example/mad_project/library.java
@@ -2,16 +2,42 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class library extends AppCompatActivity {
 
-    //Hi Abbas here
-    //Abbas here.
+    ImageButton imb1, imb2;
+    Spinner spn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_inital_page);
+
+        imb1= (ImageButton) findViewById(R.id.imb1);
+        imb2= (ImageButton) findViewById(R.id.imb2);
+        spn= (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.spinner_items_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn.setAdapter(adapter);
+    }
+
+    //public void qr(View view)
+    {
+        //Intent i=new Intent(library.this, qr.class);
+       // startActivity(i);
+    }
+    //public void book(View view)
+    {
+      //  Intent i=new Intent(library.this, book.class);
+       // startActivity(i);
     }
 }
