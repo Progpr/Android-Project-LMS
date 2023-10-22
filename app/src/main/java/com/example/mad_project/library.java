768a255bacbbdd7bc9587deed91eb3aca50b15fb
Abbas Merchant <abbasmerchant60@gmail.com>
2023-10-22 19:38:02 +0530

    Abbas created new classes for each function for spinner and also created the UI in act_book.xml. Will continue working soon

--- app/src/main/java/com/example/mad_project/library.java
+++ app/src/main/java/com/example/mad_project/library.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class library extends AppCompatActivity {
 
@@ -24,15 +26,42 @@ public class library extends AppCompatActivity {
         imb2= (ImageButton) findViewById(R.id.imb2);
         spn= (Spinner) findViewById(R.id.spinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.spinner_items_array, android.R.layout.simple_spinner_item);
+        String[] sections={"Hello User","History", "Due Amount to be Paid", "Check Latest books", "Search for books."};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sections);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spn.setAdapter(adapter);
+
+        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedSection = (String) parentView.getItemAtPosition(position);
+                if (selectedSection.equals("Hello User")) {
+                    Toast.makeText(library.this, "Hello User selected", Toast.LENGTH_SHORT).show();
+                } else if (selectedSection.equals("History")) {
+                    Intent intent = new Intent(library.this, HistoryActivity.class);
+                    startActivity(intent);
+                } else if (selectedSection.equals("Due Amount to be Paid")) {
+                    Intent intent = new Intent(library.this, DueAmountActivity.class);
+                    startActivity(intent);
+                } else if (selectedSection.equals("Check Latest books")) {
+                    Intent intent = new Intent(library.this, LatestBooksActivity.class);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here if no item is selected
+            }
+        });
+    }
+    public void book(View view) {
+        try {
+            Intent i = new Intent(library.this, main_book.class);
+            startActivity(i);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error occurred: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void book(View view)
-    {
-      Intent i=new Intent(library.this, main_book.class);
-      startActivity(i);
-    }
 }
--- app/src/main/java/com/example/mad_project/main_book.java
+++ app/src/main/java/com/example/mad_project/main_book.java
@@ -1,8 +1,12 @@
 package com.example.mad_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +24,36 @@ public class main_book extends AppCompatActivity
             ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_items_array, android.R.layout.simple_spinner_item);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spn2.setAdapter(adapter);
+            spn2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                    String selectedSection = (String) parentView.getItemAtPosition(position);
+
+                    // Open corresponding activity based on the selected item
+                    if (selectedSection.equals("Hello User")) {
+                        // Do something specific for "Hello User"
+                        // For example, show a toast message
+                        Toast.makeText(main_book.this, "Hello User selected", Toast.LENGTH_SHORT).show();
+                    } else if (selectedSection.equals("History")) {
+                        // Open History activity
+                        Intent intent = new Intent(main_book.this, HistoryActivity.class);
+                        startActivity(intent);
+                    } else if (selectedSection.equals("Due Amount to be Paid")) {
+                        // Open Due Amount activity
+                        Intent intent = new Intent(main_book.this, DueAmountActivity.class);
+                        startActivity(intent);
+                    } else if (selectedSection.equals("Check Latest books")) {
+                        // Open Latest Books activity
+                        Intent intent = new Intent(main_book.this, LatestBooksActivity.class);
+                        startActivity(intent);
+                    }
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parentView) {
+                    // Do nothing here if no item is selected
+                }
+            });
+        }
         }
 
-    }
 
